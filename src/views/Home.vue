d1f76a68a01b91b4a75446cd5cce917a75f74598
Horpey <adeniran1059@gmail.com>
2021-03-21 15:30:19 +0100

    Implement Home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,6 +4,51 @@
       <div class="col-md-5">
         <div>
           <h1>Dashboard</h1>
+          <vs-tabs class="mt-4">
+            <vs-tab label="Total Wallet">
+              <vs-card class="my-4">
+                <div slot="header">
+                  <h3>Hello world !</h3>
+                </div>
+                <div>
+                  <span
+                    >Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                    sed do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip ex ea commodo
+                    consequat.</span
+                  >
+                </div>
+              </vs-card>
+            </vs-tab>
+            <vs-tab label="Withdrawal">
+              <div class="con-tab-ejemplo">Service</div>
+            </vs-tab>
+            <vs-tab label="Balance">
+              <div class="con-tab-ejemplo">login</div>
+            </vs-tab>
+          </vs-tabs>
+          <hr class="my-5 opacity-25" />
+          <vs-list-item title="Transactions">
+            <vs-button type="flat" color="danger">See all</vs-button>
+          </vs-list-item>
+          <vs-list>
+            <vs-list-item
+              class="px-0 my-3"
+              title="Paid out #55,800 to Account Buba"
+              subtitle="Apr 28 at 22:45"
+            >
+              <p class="text-gray text-small">- 55,800.00</p>
+            </vs-list-item>
+
+            <vs-list-item
+              class="px-0 my-3"
+              title="Deposit from Bank"
+              subtitle="Feb 29 at 22:45"
+            >
+              <p class="text-gray text-small">+ 86,800.00</p>
+            </vs-list-item>
+          </vs-list>
         </div>
       </div>
       <div class="col-md-7">
